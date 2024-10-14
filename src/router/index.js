f05b5ba72e5772817e4848902357749870050e86
Maxime <maxime.boulle@efrei.net>
2024-10-14 22:29:33 +0200

    Ex 8: add guards for authenticated routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,38 @@ import { createRouter, createMemoryHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
 import ConversationIndexPage from '../pages/ConversationIndexPage.vue'
 
-const routes = [
-    { path: '/', component: HomePage },
-    { path: '/conversations', component: ConversationIndexPage },
-    { path: '/conversations/:id', component: ConversationIndexPage },
-  ]
-  
-  const router = createRouter({
-    history: createMemoryHistory(),
-    routes,
-  })
+import store from '../main.js';
 
-export default router; 
+const routes = [
+  { path: '/', component: HomePage },
+  { 
+    path: '/conversations', 
+    component: ConversationIndexPage, 
+    meta: { requiresAuth: true }
+  },
+  { 
+    path: '/conversations/:id', 
+    component: ConversationIndexPage, 
+    meta: { requiresAuth: true } 
+},
+];
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    const isAuthenticated = store.state.user !== null;
+    if (!isAuthenticated) {
+      next({ path: '/' });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
